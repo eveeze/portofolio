9777c93dab17428eb239a67f9064a38139736fb0
eveeze <jsah737@gmail.com>
2024-08-16 23:00:56 +0700

    project page ongoing

--- src/app/project/page.jsx
+++ src/app/project/page.jsx
@@ -3,6 +3,7 @@ export default function Project() {
     <>
       <div className="min-h-dvh w-full mx-auto max-w-screen-md mt-32">
         <h1 className="text-9xl text-white mt-24">hello world</h1>
+        <p>saya ingin ngepush di vgithub awkoawkoawkokoawokawkoawkoawko</p>
       </div>
     </>
   );
