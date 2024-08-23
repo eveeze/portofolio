d73b683407f75acc779a0f098fc49636bb1ffa00
eveeze <jsah737@gmail.com>
2024-08-23 22:56:12 +0700

    streakk

--- src/app/(dashboard)/dashboard/page.jsx
+++ src/app/(dashboard)/dashboard/page.jsx
@@ -5,6 +5,7 @@ export default function Dashboard() {
     <>
       <div className="mt-32 max-w-screen-md min-h-dvh mx-auto">
         <h1 className="text-white text-9xl">hello world</h1>
+        <h2>i wnatn to make an commit</h2>
       </div>
     </>
   );
